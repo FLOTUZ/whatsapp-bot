7bb2865b9fbe0806aebd349722e14a3aeac3f1ec
Emmanuel Esquivel <flotuz10@gmail.com>
2025-04-29 22:32:59 -0600

    - Add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import router from "./routes/routes";
 
@@ -7,6 +8,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use("/", router);
 
